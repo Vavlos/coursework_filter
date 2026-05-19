95437ebe711f7277ac1514ac05745c2268a87fd4
Noviko_PK <novikovpav2015@yandex.ru>
2026-05-19 22:35:13 +0300

    one_section

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
+
 #include <libopencm3/cm3/nvic.h>
+
 #include <libopencm3/stm32/adc.h>
 #include <libopencm3/stm32/dac.h>
 #include <libopencm3/stm32/gpio.h>
@@ -9,59 +11,66 @@
 #include <libopencm3/stm32/usart.h>
 #include <libopencm3/stm32/timer.h>
 
-#define LED_PORT GPIOD
-#define LED_PIN GPIO12
-#define USART_CONSOLE USART2
+#define LED_PORT       GPIOD
+#define LED_PIN        GPIO12
+#define USART_CONSOLE  USART2
 
-// ===== FILTER =====
-float b0_1=1.0f, b1_1=0.0f, b2_1=-1.0f;
-float a1_1=-1.7741107f, a2_1=0.85366075f;
-float g1=0.1080308f;
+// =====================================================
+// BANDPASS FILTER
+// =====================================================
 
-float b0_2=1.0f, b1_2=0.0f, b2_2=-1.0f;
-float a1_2=-1.9491102f, a2_2=0.9558400f;
-float g2=0.1080308f;
+// Numerator
+static const float b0 =  0.013202172f;
+static const float b1 =  0.0f;
+static const float b2 = -0.026404344f;
+static const float b3 =  0.0f;
+static const float b4 =  0.013202172f;
 
-float g3=0.8035261f;
+// Denominator
+static const float a1 = -3.6585689f;
+static const float a2 =  5.0865269f;
+static const float a3 = -3.1905211f;
+static const float a4 =  0.7630810f;
 
-float x1_1=0, x2_1=0, y1_1=0, y2_1=0;
-float x1_2=0, x2_2=0, y1_2=0, y2_2=0;
+// Input history
+static float x1 = 0.0f;
+static float x2 = 0.0f;
+static float x3 = 0.0f;
+static float x4 = 0.0f;
 
-float biquad1(float x)
+// Output history
+static float y_1 = 0.0f;
+static float y_2 = 0.0f;
+static float y_3 = 0.0f;
+static float y_4 = 0.0f;
+
+// =====================================================
+// FILTER
+// =====================================================
+
+static float filter(float x)
 {
-    float y = b0_1*x + b1_1*x1_1 + b2_1*x2_1
-            - a1_1*y1_1 - a2_1*y2_1;
+    float y =b0 * x  + b1 * x1 + b2 * x2 + b3 * x3 + b4 * x4 -a1 * y_1 - a2 * y_2 - a3 * y_3 - a4 * y_4;
 
-    x2_1 = x1_1;
-    x1_1 = x;
-    y2_1 = y1_1;
-    y1_1 = y;
+    // Shift input samples
+    x4 = x3;
+    x3 = x2;
+    x2 = x1;
+    x1 = x;
+
+    // Shift output samples
+    y_4 = y_3;
+    y_3 = y_2;
+    y_2 = y_1;
+    y_1 = y;
+
     return y;
 }
 
-float biquad2(float x)
-{
-    float y = b0_2*x + b1_2*x1_2 + b2_2*x2_2
-            - a1_2*y1_2 - a2_2*y2_2;
+// =====================================================
+// printf support
+// =====================================================
 
-    x2_2 = x1_2;
-    x1_2 = x;
-    y2_2 = y1_2;
-    y1_2 = y;
-    return y;
-}
-
-float filter(float x)
-{
-    float s = x * g1;
-    s = biquad1(s);
-    s = s * g2;
-    s = biquad2(s);
-    s = s * g3;
-    return s;
-}
-
-// ===== printf support =====
 int _write(int file, char *ptr, int len)
 {
     (void)file;
@@ -69,136 +78,271 @@ int _write(int file, char *ptr, int len)
     for (int i = 0; i < len; i++) {
         usart_send_blocking(USART_CONSOLE, ptr[i]);
     }
+
     return len;
 }
 
-// ===== ADC =====
+// =====================================================
+// ADC
+// =====================================================
+
 static uint16_t read_adc(uint8_t ch)
 {
-    uint8_t chn[1] = {ch};
-    adc_set_regular_sequence(ADC1, 1, chn);
+    uint8_t channels[1] = { ch };
+
+    adc_set_regular_sequence(ADC1, 1, channels);
+
     adc_start_conversion_regular(ADC1);
+
     while (!adc_eoc(ADC1));
+
     return (uint16_t)adc_read_regular(ADC1);
 }
 
-// ===== TIMER ISR =====
-//прерывание
+// =====================================================
+// TIMER ISR
+// =====================================================
+
 void tim2_isr(void)
 {
     if (timer_get_flag(TIM2, TIM_SR_UIF)) {
+
         timer_clear_flag(TIM2, TIM_SR_UIF);
 
+        // ADC read
         uint16_t input = read_adc(0);
 
-        // 0..4095 → -1..1
-        float x = ((float)input / 4095.0f) * 2.0f - 1.0f;
+        // Convert 0..4095 -> -1..1
+        float x =
+            ((float)input / 4095.0f) * 2.0f - 1.0f;
 
+        // Filter
         float y = filter(x);
 
-        // обратно в 0..1
+        // Convert back -1..1 -> 0..1
         y = (y + 1.0f) * 0.5f;
 
-        // защита (минимальная)
-        if (y < 0) y = 0;
-        if (y > 1) y = 1;
+        // Clipping protection
+        if (y < 0.0f)
+            y = 0.0f;
 
-        uint16_t out = (uint16_t)(y * 4095.0f);
+        if (y > 1.0f)
+            y = 1.0f;
 
-        dac_load_data_buffer_single(DAC1, out,
-            DAC_ALIGN_RIGHT12, DAC_CHANNEL2);
-        dac_software_trigger(DAC1, DAC_CHANNEL2);
+        // DAC value
+        uint16_t out =
+            (uint16_t)(y * 4095.0f);
 
+        // DAC output
+        dac_load_data_buffer_single(
+            DAC1,
+            out,
+            DAC_ALIGN_RIGHT12,
+            DAC_CHANNEL2
+        );
+
+        dac_software_trigger(
+            DAC1,
+            DAC_CHANNEL2
+        );
+
+        // LED toggle
         gpio_toggle(LED_PORT, LED_PIN);
     }
 }
 
-// ===== SETUP =====
+// =====================================================
+// CLOCK
+// =====================================================
+
 static void clock_setup(void)
 {
-    rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+    rcc_clock_setup_pll(
+        &rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]
+    );
 
     rcc_periph_clock_enable(RCC_GPIOD);
     rcc_periph_clock_enable(RCC_GPIOA);
+
     rcc_periph_clock_enable(RCC_USART2);
+
     rcc_periph_clock_enable(RCC_ADC1);
+
     rcc_periph_clock_enable(RCC_DAC);
+
     rcc_periph_clock_enable(RCC_TIM2);
 }
 
+// =====================================================
+// GPIO
+// =====================================================
+
 static void gpio_setup(void)
 {
-    gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+    gpio_mode_setup(
+        LED_PORT,
+        GPIO_MODE_OUTPUT,
+        GPIO_PUPD_NONE,
+        LED_PIN
+    );
 }
+
+// =====================================================
+// ADC SETUP
+// =====================================================
 
 static void adc_setup(void)
 {
-    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0);
+    gpio_mode_setup(
+        GPIOA,
+        GPIO_MODE_ANALOG,
+        GPIO_PUPD_NONE,
+        GPIO0
+    );
 
     adc_power_off(ADC1);
+
     adc_disable_scan_mode(ADC1);
-    adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_3CYC);
+
+    // Bigger sample time = less noise
+    adc_set_sample_time_on_all_channels(
+        ADC1,
+        ADC_SMPR_SMP_84CYC
+    );
+
     adc_power_on(ADC1);
 }
 
+// =====================================================
+// DAC SETUP
+// =====================================================
+
 static void dac_setup(void)
 {
-    gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO5);
+    gpio_mode_setup(
+        GPIOA,
+        GPIO_MODE_ANALOG,
+        GPIO_PUPD_NONE,
+        GPIO5
+    );
 
     dac_disable(DAC1, DAC_CHANNEL2);
+
     dac_enable(DAC1, DAC_CHANNEL2);
 }
 
+// =====================================================
+// USART
+// =====================================================
+
 static void usart_setup(void)
 {
-    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
+    gpio_mode_setup(
+        GPIOA,
+        GPIO_MODE_AF,
+        GPIO_PUPD_NONE,
+        GPIO2
+    );
+
     gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
 
-    usart_set_baudrate(USART_CONSOLE, 115200);
-    usart_set_databits(USART_CONSOLE, 8);
-    usart_set_stopbits(USART_CONSOLE, USART_STOPBITS_1);
-    usart_set_parity(USART_CONSOLE, USART_PARITY_NONE);
-    usart_set_flow_control(USART_CONSOLE, USART_FLOWCONTROL_NONE);
-    usart_set_mode(USART_CONSOLE, USART_MODE_TX);
+    usart_set_baudrate(
+        USART_CONSOLE,
+        115200
+    );
+
+    usart_set_databits(
+        USART_CONSOLE,
+        8
+    );
+
+    usart_set_stopbits(
+        USART_CONSOLE,
+        USART_STOPBITS_1
+    );
+
+    usart_set_parity(
+        USART_CONSOLE,
+        USART_PARITY_NONE
+    );
+
+    usart_set_flow_control(
+        USART_CONSOLE,
+        USART_FLOWCONTROL_NONE
+    );
+
+    usart_set_mode(
+        USART_CONSOLE,
+        USART_MODE_TX
+    );
 
     usart_enable(USART_CONSOLE);
 }
 
-// ===== TIMER =====
+// =====================================================
+// TIMER
+// =====================================================
+
 static void tim_setup(void)
 {
     nvic_enable_irq(NVIC_TIM2_IRQ);
 
     rcc_periph_reset_pulse(RST_TIM2);
 
-    timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT,
-        TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+    timer_set_mode(
+        TIM2,
+        TIM_CR1_CKD_CK_INT,
+        TIM_CR1_CMS_EDGE,
+        TIM_CR1_DIR_UP
+    );
 
-    timer_set_prescaler(TIM2, (rcc_apb1_frequency * 2) / 1000000);
+    // Timer clock = 1 MHz
+    timer_set_prescaler(
+        TIM2,
+        (rcc_apb1_frequency * 2) / 1000000
+    );
 
-    timer_set_period(TIM2, 1000000 / 17500);
+    // Fs = 17500 Hz
+    timer_set_period(
+        TIM2,
+        1000000 / 17500
+    );
 
-    timer_enable_irq(TIM2, TIM_DIER_UIE);
+    timer_enable_irq(
+        TIM2,
+        TIM_DIER_UIE
+    );
+
     timer_enable_counter(TIM2);
 }
 
-// ===== MAIN =====
+// =====================================================
+// MAIN
+// =====================================================
+
 int main(void)
 {
     clock_setup();
+
     gpio_setup();
+
     adc_setup();
+
     dac_setup();
+
     usart_setup();
+
     tim_setup();
 
-    printf("Fs = 17.5 kHz OK\n");
+    printf("Bandpass filter Fs = 17500 Hz\r\n");
 
-    while (1) {}
+    while (1) {
+    }
 }
 
-
-
+// =====================================================
+// STUBS FOR NEWLIB
+// =====================================================
 
 int _close(int file)
 {
